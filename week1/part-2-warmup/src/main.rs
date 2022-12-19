071802fa8a6015c027ffe1813e6dac105590095f
lishoulong <lishoulong@bytedance.com>
2022-12-19 20:23:28 +0800

    feat: week1 two tasks

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -7,11 +7,17 @@ fn main() {
 }
 
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+    let mut retv = Vec::new();
+    for i in v.iter() {
+        retv.push(i + n);
+    }
+    retv
 }
 
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
-    unimplemented!()
+    for i in 0..v.len() {
+        v[i] = v[i] + n;
+    }
 }
 
 fn dedup(v: &mut Vec<i32>) {
